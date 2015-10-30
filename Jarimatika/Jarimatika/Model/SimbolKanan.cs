044bdf89d0592d16d7ff58a450fbe9521b34b0f6
DavisNur <davis.fandi@gmail.com>
2015-10-30 22:47:02 +0700

    Adaptive Layout SimbolPage

---
+++ Jarimatika/Jarimatika/Model/SimbolKanan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jarimatika.Models
+{
+    class SimbolKanan
+    {
+        public string GambarJari { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
